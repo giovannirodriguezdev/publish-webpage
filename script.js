0507e6fc9bc07ca59f10b255d193d5052986e5df
Giovanni Rodriguez <113161034+giovannirodriguezdev@users.noreply.github.com>
2024-08-23 09:12:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ writeRandomQuote = function () {
     "Patience is bitter, but its fruit is sweet - Aristotle",
     "Difficult things in the world, must have their beginnings in the easy. - Lao Tzu",
     "The two most powerful warriors are patience and time. - Leo Tolstoy",
-    "It is easier to find men who will volunteer to die. Than to find those 
-        who are willing to endure pain with patience. - Julius Caesar"
+    "It is easier to find men who will volunteer to die. Than to find those who are willing to endure pain with patience. - Julius Caesar"
   ]
     let rand = Math.floor(Math.random()*quotes.length);
     document.getElementById("quote").innerText = quotes[rand];
